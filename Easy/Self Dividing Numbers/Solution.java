24ecf906ecd3cc3f990c7003a8f07eee0bb3ac5b
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-07-28 12:30:22 +0530

    Sync: Self Dividing Numbers (Java)

---
+++ Easy/Self Dividing Numbers/Solution.java
@@ -0,0 +1,17 @@
+temp=temp/10;
+            }
+            return true;
+    }
+                }
+                    return false;
+                {
+                if(rem==0 || num%rem!=0)
+                int rem=temp%10;
+            {
+            while(temp!=0)
+    public List<Integer> selfDividingNumbers(int left, int 
+    right) {
+        List<Integer> l=new ArrayList<>();
+        for(int i=left; i<=right; i++)
+        {
+            if(solve(i))
