0e30a6942f8c10a72dd65be9e9cd22939488c3eb
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-07-30 01:53:27 +0530

    Sync: Sum Multiples (Java)

---
+++ Easy/Sum Multiples/Solution.java
@@ -0,0 +1,16 @@
+sum+=i;
+            }
+            else if(i%5==0)
+            {
+                sum+=i;
+            }
+            else if(i%7==0)
+            {
+                sum+=i;
+            {
+            if(i%3==0)
+        {
+        for(int i=1; i<=n; i++)
+        int sum=0;
+    public int sumOfMultiples(int n) {
+class Solution {
