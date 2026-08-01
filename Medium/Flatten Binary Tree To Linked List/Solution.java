c4b93266ecd24256c6455b2660072a9580729f06
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-01 14:22:09 +0530

    Sync: Flatten Binary Tree To Linked List (Java)

--- Medium/Flatten Binary Tree To Linked List/Solution.java
+++ Medium/Flatten Binary Tree To Linked List/Solution.java
@@ -1,12 +1,4 @@
-*         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    TreeNode pre=null;
-    public void flatten(TreeNode root) {
-        // TreeNode curr=root;
+// TreeNode curr=root;
         
         if(root==null)
         {
@@ -15,3 +7,10 @@ class Solution {
         flatten(root.right);
         flatten(root.left);
         root.right=pre;
+        pre=root;
+        root.left=null;
+        
+
+        pre= root;
+    }
+}
