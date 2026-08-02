c1590319a56794a0728c826d02a460c8dd970b2e
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-02 23:59:34 +0530

    Sync: Min Cost Climbing Stairs (Java)

---
+++ Easy/Min Cost Climbing Stairs/Solution.java
@@ -0,0 +1,17 @@
+{
+        if((cost[idx]+cost[idx+1]>cost[idx]+cost[idx+2])
+    }
+
+
+    public int minCostClimbingStairs(int[] cost) {
+        
+        {
+            return mn=cost[idx]+cost[idx+2];
+        }
+        else
+        {
+            return mn=cost[idx]+cost[idx+1];
+        }
+        return solve(idx, cost, mn);
+        int idx=0;
+        int mn=Integer.MAX_VALUE;
