0d8944e8d0d414b80af480d927b3eac38bec3b19
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-04 21:51:12 +0530

    Sync: Find Missing Elements (Java)

--- Easy/Find Missing Elements/Solution.java
+++ Easy/Find Missing Elements/Solution.java
@@ -1,18 +1,5 @@
-int n=nums.length;
-        Arrays.sort(nums);
-        List<Integer> arr=new ArrayList<>(n);
-        int t=nums[n-1]-nums[0]+1;
-        for(int i=nums[0]; i<nums[n-1]; i++)
-        {
-            // if(nums[i]!=nums[i+1]-1)
-            // {
-            //     
-            // }
-            if(i+1!=nums[n-1])
-            {
-                arr.add(i+1);
-            }
-        }
-        if(t-n==arr.size())
-        {
-            return arr;
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        
+    }
+}
