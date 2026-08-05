7d78a43c361229a6ca8d7124ddb73c1793ae8ad7
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-05 09:46:03 +0530

    Sync: Find Missing Elements (Java)

--- Easy/Find Missing Elements/Solution.java
+++ Easy/Find Missing Elements/Solution.java
@@ -1,5 +1,16 @@
-class Solution {
+{
+            hs.add(num);
+        }
+        for(int i=nums[0]; i<; i++)
+        {
+            if(!hs.contains(i))
+            {
+                arr.add(i);
+
+            }
+        for(int num: nums)
+        List<Integer> arr= new ArrayList<>();
+        Set<Integer> hs=new HashSet<>();
+        int n=nums.length;
     public List<Integer> findMissingElements(int[] nums) {
-        
-    }
-}
+class Solution {
