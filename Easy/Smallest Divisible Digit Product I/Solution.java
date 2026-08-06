cfe88cb6f81705837a5c955e6e2197470431c93f
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-06 14:52:46 +0530

    Sync: Smallest Divisible Digit Product I (Java)

--- Easy/Smallest Divisible Digit Product I/Solution.java
+++ Easy/Smallest Divisible Digit Product I/Solution.java
@@ -1,6 +1,11 @@
 class Solution {
     public int smallestNumber(int n, int t) {
-        
+        int 
+        while(n!=0)
+        {
+             int rem=n%10;
+             n/=10;
+        }
     }
-        
+
 }
