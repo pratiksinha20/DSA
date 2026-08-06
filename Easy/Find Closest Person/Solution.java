4e89e0b2abc5cb7eb28734e877fefe75252a3656
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-06 22:58:52 +0530

    Sync: Find Closest Person (Java)

--- Easy/Find Closest Person/Solution.java
+++ Easy/Find Closest Person/Solution.java
@@ -1,5 +1,17 @@
 class Solution {
     public int findClosest(int x, int y, int z) {
+        if(Math.abs(x-z)>Math.abs(y-z))
+        {
+            return 2;
+        }
+        else if(Math.abs(y-z)>Math.abs(x-z))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
         
     }
 }
