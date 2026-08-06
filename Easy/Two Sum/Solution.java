d48d2eb4f8974d18dc1eb4bcd00db753d82dad2f
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-06 22:59:47 +0530

    Sync: Two Sum (Java)

--- Easy/Two Sum/Solution.java
+++ Easy/Two Sum/Solution.java
@@ -1,11 +1,3 @@
-// #include<iostream>
-// using namespace std;
-
-int main()
-{
-int n;
-cout<<"number of element is:";
-cin>>n;
 cout<<"enter your array";
 int arr[n];
 for(int i=0; i<n; i++)
@@ -22,3 +14,11 @@ for(int i=0; i<n; i++)
     {
       if(arr[i]+arr[j]==target)
       {
+cin>>n;
+cout<<"number of element is:";
+int n;
+{
+int main()
+
+// using namespace std;
+// #include<iostream>
