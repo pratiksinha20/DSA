e663aee5ba314f4671d0266485478d1749149e16
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-06 22:58:09 +0530

    Sync: Maximum Product Of Two Elements In An Array (Java)

--- Easy/Maximum Product Of Two Elements In An Array/Solution.java
+++ Easy/Maximum Product Of Two Elements In An Array/Solution.java
@@ -1,5 +1,9 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        
+        Arrays.sort(nums);
+        int n=nums.length;
+        int mxValue=(nums[n-1]-1)*(nums[n-2]-1);
+
+        return mxValue;    
     }
 }
