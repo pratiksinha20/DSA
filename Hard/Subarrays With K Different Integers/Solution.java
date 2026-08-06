c937015ccd58397611b89ac46cf701fdefc2eb0e
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-06 12:58:20 +0530

    Sync: Subarrays With K Different Integers (Java)

--- Hard/Subarrays With K Different Integers/Solution.java
+++ Hard/Subarrays With K Different Integers/Solution.java
@@ -1,4 +1,11 @@
-while (freq.size() > k) {
+class Solution {
+    private int atMostK(int[] nums, int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int l = 0, ans = 0;
+        for (int r = 0; r < nums.length; r++) {
+            freq.put(nums[r], freq.getOrDefault(nums[r], 0) + 1);
+
+            while (freq.size() > k) {
                 freq.put(nums[l], freq.get(nums[l]) - 1);
                 if (freq.get(nums[l]) == 0) {
                     freq.remove(nums[l]);
@@ -7,10 +14,3 @@ while (freq.size() > k) {
             }
             ans += (r - l + 1);
         }
-        return ans;
-    }
-
-    public int subarraysWithKDistinct(int[] nums, int k) {
-        return atMostK(nums, k) - atMostK(nums, k - 1);
-    }
-}
