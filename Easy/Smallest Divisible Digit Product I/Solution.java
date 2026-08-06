af929054ce308b3329156d54b977cb047b8d0a1c
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-06 15:17:50 +0530

    Sync: Smallest Divisible Digit Product I (Java)

--- Easy/Smallest Divisible Digit Product I/Solution.java
+++ Easy/Smallest Divisible Digit Product I/Solution.java
@@ -1,9 +1,18 @@
-class Solution {
-    public int smallestNumber(int n, int t) {
-        int mul=1;
-        int i=n;
-        
+int mul = 1;
+        int i = n;
+        while(i<100)
+        {
+            while(i=0)
+            {
+                int rem=i%10;
+                i=i/10;
+                mul=mul*rem;
+            }
+            if(mul%t!=0)
+            {
+                i++;
+            }
+        }
+
     }
-
-
 }
