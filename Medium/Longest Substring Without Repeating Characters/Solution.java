c0e2c0cd71f829bbf78664633e46712271768b3f
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-06 22:45:45 +0530

    Sync: Longest Substring Without Repeating Characters (Java)

--- Medium/Longest Substring Without Repeating Characters/Solution.java
+++ Medium/Longest Substring Without Repeating Characters/Solution.java
@@ -1,10 +1,12 @@
-l=Math.max(l, hash[s.charAt(r)]+1);
-            }
-            hash[s.charAt(r)]=r;
-
-            ans=Math.max(ans, r-l+1);
-            r++;
-        }
-       return ans; 
-    }
-}
+{
+        while(r<s.length())
+        Arrays.fill(hash, -1);
+            if(hash[s.charAt(r)]!=-1)
+        int[] hash=new int[256];
+        // Arrays.fill(hash, -1);
+        // List<Integer> hash=new ArrayList<>();
+        int ans=Integer.MIN_VALUE;
+        int r=0;
+        int l=0;
+    public int lengthOfLongestSubstring(String s) {
+class Solution {
