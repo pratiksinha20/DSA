666b84c8511333a33de27de0e22b288dc3659dc3
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-07 14:24:10 +0530

    Sync: Longest Consecutive Sequence (Java)

--- Medium/Longest Consecutive Sequence/Solution.java
+++ Medium/Longest Consecutive Sequence/Solution.java
@@ -1,15 +1,16 @@
-}
-            
-            mx=Math.max(mx, count);
+mx=Math.max(mx, count);
+        }
         if(n==0)
         {
             return 0;
         }
-        return mx+1;
-        if else(n==1)
+        else if(n==1)
         {
             return 1;
         }
-        
+        return mx+1;
+
+
+
+         
     }
-}
