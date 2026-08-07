a7a1bae66eb7d163907445d020331031a8a620e6
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-07 14:06:28 +0530

    Sync: Group Anagrams (Java)

--- Medium/Group Anagrams/Solution.java
+++ Medium/Group Anagrams/Solution.java
@@ -1,16 +1,17 @@
-String st=new String(c);
+String temp=strs[i];
+            char[] c=temp.toCharArray();
+            Arrays.sort(c);
+            String st=new String(c);
             String org=temp;
-        }
-        List<List<String>> ans=new ArrayList<>(mp.values());
+
             if(!mp.containsKey(st))
             {
-
-            }
                 mp.put(st, new ArrayList<>());
 
-            Arrays.sort(c);
-            char[] c=temp.toCharArray();
-            String temp=strs[i];
+            }
+            mp.get(st).add(temp);
+        }
+        List<List<String>> ans=new ArrayList<>(mp.values());
         return ans;
     }
 }
