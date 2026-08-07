d1543a6bc6c4af8302e00793c83f1a9df871c1c2
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-07 14:17:41 +0530

    Sync: Longest Consecutive Sequence (Java)

--- Medium/Longest Consecutive Sequence/Solution.java
+++ Medium/Longest Consecutive Sequence/Solution.java
@@ -1,5 +1,5 @@
 {
-            if(nums[i]==nums[i]-1)
+            if(nums[i]==nums[i+1]-1)
         }
             {
                 count++;
@@ -8,5 +8,9 @@
         
             mx=Math.max(mx, count);
         return mx;
-    }
-}
+        for(int i=0; i<nums.length; i++)
+        int mx=Integer.MIN_VALUE;
+        int count=0;
+        Arrays.sort(nums);
+    public int longestConsecutive(int[] nums) {
+class Solution {
