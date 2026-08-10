c4ea4534bbda81238c8462d159a4d803f6499501
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-10 15:38:19 +0530

    Sync: Sliding Window Maximum (Java)

--- Hard/Sliding Window Maximum/Solution.java
+++ Hard/Sliding Window Maximum/Solution.java
@@ -1,13 +1,18 @@
-for(int i=0; i<k; i++)
-        {
-            pq.add({nums[i], i});
-        }
-
-        li.add(pq.peek().first);
+li.add(pq.peek().first);
 
         for(int i=k; i<n; i++)
         {
             pq.add({nums[i], i});
         }
 
-            if(!pq.empty() &&pq.peek().first<i-k+1)
+            while(!pq.empty() &&pq.peek().first<i-k+1)
+            {
+                pq.poll();
+            }
+            li.add(pq.peek().first);
+
+
+
+
+    }
+        return
