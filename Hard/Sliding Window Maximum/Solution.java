3d199f3e71ceff8709bccf9fa1b44c839edb3e96
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-10 15:31:22 +0530

    Sync: Sliding Window Maximum (Java)

--- Hard/Sliding Window Maximum/Solution.java
+++ Hard/Sliding Window Maximum/Solution.java
@@ -1,5 +1,16 @@
-class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        
+List<Integer> li=new ArrayList<>();
+
+
+
+        for(int i=0; i<k; i++)
+        {
+            pq.add({nums[i], i});
+        }
+
+        li.add(pq.peek().first);
+
+        for(int i=k; i<n; i++)
+
+
     }
 }
