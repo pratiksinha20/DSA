55fcb15eba490b0b6d72262152e3e3c49302f6eb
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-11 10:02:22 +0530

    Sync: Design Hashmap (Java)

--- Easy/Design Hashmap/Solution.java
+++ Easy/Design Hashmap/Solution.java
@@ -1,21 +1,21 @@
 class MyHashMap {
+    List<Integer> li=new ArrayList<>();
 
     public MyHashMap() {
-        
+        List<Integer> arr=new ArrayList<>(Collections.nCopies
+        ((int)1e6+7, -1));
+        li=arr;
     }
     
     public void put(int key, int value) {
+        li.set(key, value);
         
     }
     
     public int get(int key) {
+        int ans=li.get(key);
+        return ans;
         
     }
     
     public void remove(int key) {
-        
-    }
-}
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
