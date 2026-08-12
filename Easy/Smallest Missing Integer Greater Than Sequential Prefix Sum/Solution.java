6a49e5b065140a0a7e039f5e933b87807596daee
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-13 02:45:24 +0530

    Sync: Smallest Missing Integer Greater Than Sequential Prefix Sum (Java)

--- Easy/Smallest Missing Integer Greater Than Sequential Prefix Sum/Solution.java
+++ Easy/Smallest Missing Integer Greater Than Sequential Prefix Sum/Solution.java
@@ -1,11 +1,21 @@
-class Solution {
-    public int missingInteger(int[] nums) {
-        int sum=0;
-        for(int i=0; i<nums.length; i++)
-        {
-            if()
-        }
-
- 
-    }
+1class Solution {
+public int missingInteger(int[] nums) {
+HashSet<Integer> set=new HashSet<>();
+int sum=nums[0];
+int i=0;
+while(i<nums.length-1 && nums[i]+1==nums[i+1])
+{
+sum+=nums[i+1];
+i++;
 }
+for(int num: nums)
+{
+set.add(num);
+}
+while(set.contains(sum))
+{
+sum++;
+}
+return sum;
+}
+24}
