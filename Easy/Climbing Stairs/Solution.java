bed8d69bfa9a8db56ef2b5be6a542abd9ee73cee
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-17 18:42:45 +0530

    Sync: Climbing Stairs (Java)

--- Easy/Climbing Stairs/Solution.java
+++ Easy/Climbing Stairs/Solution.java
@@ -1,18 +1,5 @@
-int solve(int n, int[] dp)
-    {
-        if(n<=3) return n;
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-        return dp[n]=solve(n-1, dp)+solve(n-2, dp);
-    }
-
-
+class Solution {
     public int climbStairs(int n) {
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n, dp);
-   
+        
     }
 }
