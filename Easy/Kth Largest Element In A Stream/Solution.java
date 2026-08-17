f877fd2d7d7a9e6b3e8c7434fec42527285ebf4a
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-17 11:06:05 +0530

    Sync: Kth Largest Element In A Stream (Java)

--- Easy/Kth Largest Element In A Stream/Solution.java
+++ Easy/Kth Largest Element In A Stream/Solution.java
@@ -1,22 +1,17 @@
 }
     
     public int add(int val) {
-        }
         pq.add(val);
         if(pq.size()>K)
         {
             pq.poll();
         }
-            pq.poll();
-        {
-        while(pq.size()>k)
-        }
-            pq.add(nums[i]);
-        {
-        for(int i=0; i<nums.length; i++)
-    public KthLargest(int k, int[] nums) {  
-    int K=0;
-    PriorityQueue<Integer> pq=new PriorityQueue<>();
-        K=k;
         return pq.poll();  
     }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
