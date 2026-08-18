a4b3f83f6a84d2bd8be2b71a5d2df597cc7ffc71
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-18 09:38:47 +0530

    Sync: Climbing Stairs (Java)

--- Easy/Climbing Stairs/Solution.java
+++ Easy/Climbing Stairs/Solution.java
@@ -1,14 +1,19 @@
-class Solution {
-    public int climbStairs(int n) {
-        // Map<Integer, Integer> mp=new HashMap<>();
-        // if(n<=3) return n;
-        // if(mp.containsKey(n))
-        // return res;
-        
-        // {
-        //     return mp.get(n);
+//     return mp.get(n);
         // }
-    }
-        // mp.put(n,res);
         // int res=climbStairs(n-1)+climbStairs(n-2);
+        // mp.put(n,res);
+        // return res;
+        if(n<=3) return n;
+
+        int[] dp= new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        // dp[3]=3;
+        for(int i=3; i<=n; i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
 }
