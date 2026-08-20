749cc19d5bfdab6724cbf62599bf4b588877d854
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-08-20 12:19:49 +0530

    Sync: Koko Eating Bananas (Java)

--- Medium/Koko Eating Bananas/Solution.java
+++ Medium/Koko Eating Bananas/Solution.java
@@ -1,17 +1,10 @@
-while(low<=high)
-    int ans=Integer.MAX_VALUE;
-    int low=1;
-    int high=Arrays.stream(piles).max().getAsInt();
-
-    public int minEatingSpeed(int[] piles, int h) {
-    
-    }
-        return total<=h;
+high=mid-1;
         }
-            total+=Math.ceil((double)piles[i]/mid);
+        else
         {
-        for(int i=0; i<piles.length; i++)
-        int total=0;
-        boolean poss(int[] piles, int mid, int h )
-    {
-class Solution {
+            low=mid+1;
+        }
+    }
+      return ans;
+    }
+}
