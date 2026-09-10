e1626b72243a37c3a01e256c7c3ae44a68597ab1
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-09-11 00:56:33 +0530

    Sync: Palindromic Substrings (Java)

---
+++ Medium/Palindromic Substrings/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n= s.length();
+        int count=0;
+        for(int i=0; i<n; i++ )
+        {
+    boolean isPalindrome(String s, int i, int j)
+    {
+    while(i<j ){
+    }
+    if(s.charAt(i)!=s.charAt(j))
+    {
+    return false;
+    }
+    i++;
+    j--;
+
+    }
+    return true;
